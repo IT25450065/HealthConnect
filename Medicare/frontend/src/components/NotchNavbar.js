0fed3f24bc97c372a4cc55dd05087a07732c3681
IT25450065 <it25450065@my.sliit.lk>
2026-09-16 02:50:52 +0530

    Delete Medicare directory

--- Medicare/frontend/src/components/NotchNavbar.js
+++
@@ -1,7 +0,0 @@
-/**
- * Reusable Apple Style Floating Notch Navbar Component
- */
-export const NotchNavbar = {
-  name: 'VengeanceUI Notch Navbar',
-  render: () => '<header class="notch-navbar-wrapper">...</header>'
-};
