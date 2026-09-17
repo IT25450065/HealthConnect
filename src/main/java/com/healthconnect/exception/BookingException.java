77abc675c84de551031a459621e2f71ded94452e
IT25450065 <it25450065@my.sliit.lk>
2026-09-17 16:20:16 +0530

    uplaoding skeleton

--- src/main/java/com/healthconnect/exception/BookingException.java
+++
@@ -1,9 +0,0 @@
-package com.healthconnect.exception;
-
-/** UC-01 / UC-04 alternate flows: conflict, invalid date, outside working hours. */
-public class BookingException extends BusinessException {
-
-    public BookingException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/healthconnect/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.healthconnect.exception;
-
-/** A row was requested by id and does not exist (usually a hand-edited URL). */
-public class ResourceNotFoundException extends BusinessException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
